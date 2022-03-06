4f6bf4d62d10967b7b9e5d7ea818d63882888450
Paul CONVERT <paul.convert@epitech.eu>
2022-03-06 20:08:24 +0100

    test hosting v10

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/angular-heroku'));
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+
-        '/dist/angular-heroku/index.html'));});
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/angular-tour-of-heroes'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/angular-tour-of-heroes'}),
+);
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
